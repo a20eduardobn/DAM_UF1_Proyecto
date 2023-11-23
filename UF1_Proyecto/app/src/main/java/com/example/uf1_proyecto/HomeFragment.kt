2b7d6efc775913143e951a0c83149b6baa83a1cc
a20eduardobn <a20eduardobn@iessanclemente.net>
2023-11-23 14:32:33 +0100

    Retoques a homeFragment

--- UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/HomeFragment.kt
+++ UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/HomeFragment.kt
@@ -9,61 +9,57 @@ import androidx.core.content.ContextCompat
 import com.example.uf1_proyecto.databinding.FragmentHomeBinding
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartFontWeightType
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
-import com.github.aachartmodel.aainfographics.aachartcreator.AAChartStackingType
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
 import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
 import com.github.aachartmodel.aainfographics.aaoptionsmodel.AAStyle
 
 class HomeFragment : Fragment() {
 
-//Implement viewbinding
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //Implementando viewbinding
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        //Creando el chart a partir del modelo de datos
+        binding.mainChartView.aa_drawChartWithChartModel(createChartModel())
+
+        // Devolver la vista
+        return view
+    }
+
+    //Funcion que crea el modelo de datos para el chart
+    private fun createChartModel(): AAChartModel {
         //Convertir los colores de colors.xml a strings para poder usarlos en el chart
-        //Colores principales de la app
-        val colorAccentOrange = "#" + Integer.toHexString(ContextCompat.getColor
-            (requireContext(), R.color.accent_color_orange)).substring(2)
-        val colorAccentYellow = "#" + Integer.toHexString(ContextCompat.getColor
-            (requireContext(), R.color.accent_color_yellow)).substring(2)
-        val colorPrimary = "#" + Integer.toHexString(ContextCompat.getColor
-            (requireContext(), R.color.colorPrimary)).substring(2)
+        val colorPrimary = "#" + Integer.toHexString(
+            ContextCompat.getColor
+                (requireContext(), R.color.colorPrimary)
+        ).substring(2)
+        val colorAccentPrimary = "#" + Integer.toHexString(
+            ContextCompat.getColor
+                (requireContext(), R.color.accent_color_primary)
+        ).substring(2)
 
-        //Creando el chart junto a su modelo de datos
-        val chartModel : AAChartModel = AAChartModel()
-            .chartType(AAChartType.Bar)
-            .title(getString(R.string.balance_mensual))
-            .titleStyle(
-                AAStyle()
-                .color(colorAccentOrange)
-                .fontSize(20f)
-                .fontWeight(AAChartFontWeightType.Bold)
-            )
-
+        //Modelo de datos para el chart
+        return AAChartModel()
+            .chartType(AAChartType.Column)
             .dataLabelsEnabled(false)
             .legendEnabled(false)
             .axesTextColor("#FFFFFF")
-            .colorsTheme(arrayOf(colorAccentYellow))
+            .colorsTheme(arrayOf(colorAccentPrimary))
             .backgroundColor(colorPrimary)
             .categories(arrayOf(getString(R.string.gastosWord), getString(R.string.ingresosWord)))
             .series(
                 arrayOf(
                     AASeriesElement()
                         .name("Temperatura")
-                        .data(arrayOf(800, 200))
+                        .data(arrayOf(420, 140))
                 )
             )
-        binding.mainChartView.aa_drawChartWithChartModel(chartModel)
-
-        // Inflate the layout for this fragment
-        return view
+            .yAxisTitle("")
     }
 
     override fun onDestroyView() {
--- UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/MainActivity.kt
+++ UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.uf1_proyecto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.uf1_proyecto.databinding.ActivityMainBinding
+import com.example.uf1_proyecto.databinding.FragmentHomeBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //Create the view binding object
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //Add the material toolbar
+        setSupportActionBar(binding.toolbar)
+        //Change the title of the toolbar
+        supportActionBar?.title = getString(R.string.balance_mensual)
     }
 }
